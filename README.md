7bc6c2c3450e4f0ecd716ae4542c0e5ae12a0fe3
Lawrence <lawrence.vanderpool@gmail.com>
2017-03-29 11:58:26 -0500

    update a few minor things

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # A stupid chrome extension to replace the R and r in Trump, Kushner, Republican, with the backwards cyrillic character that looks like an R
 
 I'm a genius, I know
+
+# Install it?
+
+1. Download this whole project. 
+2. Open up chrome://extensions in your chrome browser. 
+3. Click 'Developer Mode'. 
+4. Click 'Load Unpacked Extensions.
+5. Select the folder for this project that you downloaded.
+6. Refresh a page about Trump.
+7. Laugh like you heard a really good fart joke.
+
+# Contributing, License, Credit
+lol
--- popup.js
+++ popup.js
@@ -1,16 +1,20 @@
 var elements = document.getElementsByTagName('*');
+console.log(elements);
 
 for (var i=0; i<elements.length; i++) {
     var element = elements[i];
+
     for (var j=0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
+
         if (node.nodeType === 3) {
             var text = node.nodeValue;
             var replacedText = text.replace(/Trump|Kushner|Republican/gi, function replacer(word) {
-                return word.replace('r', '\u044F').replace('R', '\u042F')thu;
+                return word.replace('r', '\u044F').replace('R', '\u042F');
             });
 
             if (replacedText !== text) {
+                console.log('replaced');
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
